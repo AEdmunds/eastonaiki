5f8f07a5987f0d600edca0db78f71c3e851cf7b8
Ali <alistair.edmunds@gmail.com>
2012-06-01 15:25:07 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eastonaiki::Application.config.secret_token = 'ed09c282acff59b72af9f0510edb55ea891dc2e22eba486d63d02ca0a9e935d06309e8a50611e072df0825d62f94ee9a77217532126b73b54f2ca1cc638290f6'
